4f7a0ea71bbed8607a2b87c9fdbd818c9bb943fc
Nagini91 <68591817+Nagini91@users.noreply.github.com>
2021-03-02 09:57:07 +0100

    Delete contents directory

--- contents/coursDuSoir/passwordArray.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $loginArray = [
-                "admin" => "ewbfazalad8",
-                "user" => "123",
-                "Julie" => "dmadbwesf20",
-                "Directeur" => "f20dmadbwes"
-
-        ];
-?>
